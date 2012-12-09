985fa3f2a684f07fbd6037ec337daf08f1e764fc
. <.>
2012-12-09 19:57:28 +0100

    commit

--- views/studiengaenge/backend_studiengaenge.php
+++ views/studiengaenge/backend_studiengaenge.php
@@ -1,14 +1,40 @@
-<?php
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+       "http://www.w3.org/TR/html4/loose.dtd">
+<html>
 
-/**
- * FHD-App
- *
- * @version 0.0.1
- * @copyright Fachhochschule Duesseldorf, 2012
- * @link http://www.fh-duesseldorf.de
- * @author Fabian Martinovic (FM), <fabian.martinovic@fh-duesseldorf.de>
- */
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link href="../../sources/css/style.css" rel="stylesheet" type="text/css" media="screen" />
+	<script type="text/javascript" src="sources/js/jquery-1.8.2.min.js"></script>
+	<title>FHD App - Redaktion</title>
+</head>
 
- 
-/* End of file studiengaenge_edit.php */
-/* Location: ./views/studiengaenge/studiengaenge_edit.php */
+<body>
+
+	<div id="wrapper">
+		<div id="header">
+			<h1> FHD-App Redaktion </h1>
+		</div>
+		<div id="nav">
+			<ul>
+				<a href="../studiengaenge/backend_studiengaenge.php"><li>Studieng&auml;nge</li></a>
+				<a href="../mensa/backend_mensa.php"><li>Mensa</li></a>
+				<a href="../termine/backend_termine.php"><li>Termine</li></a>
+				<a href="../kontakte/backend_kontakte.php"><li>Kontakte</li></a>
+				<a href="../faq/backend_faq.php"><li>FAQ</li></a>
+				<a href="../veranstaltungen/backend_veranstaltungen.php"><li>Veranstaltungen</li></a>
+			</ul>
+		</div>
+		
+		<div id="content">
+		
+			
+		
+		</div>
+		
+		<div class="clearBoth"></div>
+	</div>	
+
+</body>
+
+</html>
