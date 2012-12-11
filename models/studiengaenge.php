22d05d69c9c2035dcd58cf71e29226d8c6ec4515
. <.>
2012-12-11 09:31:37 +0100

    commit

--- models/studiengaenge.php
+++ models/studiengaenge.php
@@ -1,14 +1,23 @@
 <?php
+	class StudycoursesModel{
+		//IV
+		private $connection;
+		
+		//Construct
+		public function __construct(){
+			$this->connection = new mysqli($_SESSION['host'], $_SESSION['user'], $_SESSION['pwd'], $_SESSION['db']);	//Instanz der Datenbankverbindung in connection abspeichern
+		}
+		
+		//IM
+		public function insertStudycourse{
+			try{
+//				$this->connection->query("INSERT INTO studycourses() VALUES()");
+			}
+			catch(Exception $e){
+				echo $e->getMessage();
+			}
+		}
+	}
 
-/**
- * FHD-App
- *
- * @version 0.0.1
- * @copyright Fachhochschule Duesseldorf, 2012
- * @link http://www.fh-duesseldorf.de
- * @author Fabian Martinovic (FM), <fabian.martinovic@fh-duesseldorf.de>
- */
 
- 
-/* End of file studiengaenge.php */
-/* Location: ./models/studiengaenge.php */
+?>
