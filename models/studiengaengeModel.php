5372c9e91641ddd5088cc68308b6ed08c0145859
. <.>
2012-12-11 09:34:21 +0100

    commit

---
+++ models/studiengaengeModel.php
@@ -0,0 +1,23 @@
+<?php
+	class StudycoursesModel{
+		//IV
+		private $connection;
+		
+		//Construct
+		public function __construct(){
+			$this->connection = new mysqli($_SESSION['host'], $_SESSION['user'], $_SESSION['pwd'], $_SESSION['db']);	//Instanz der Datenbankverbindung in connection abspeichern
+		}
+		
+		//IM
+		public function insertStudycourse{
+			try{
+//				$this->connection->query("INSERT INTO studycourses() VALUES()");
+			}
+			catch(Exception $e){
+				echo $e->getMessage();
+			}
+		}
+	}
+
+
+?>
