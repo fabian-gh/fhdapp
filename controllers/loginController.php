dc3432d237f2694af21d1254273420061788a83e
Fabian Martinovic <jimmy_crowbar@gmx.de>
2012-12-12 13:26:39 +0100

    Fehler behoben

--- controllers/loginController.php
+++ controllers/loginController.php
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-<?php
-
-/**
- * FHD-App
- *
- * @version 0.0.1
- * @copyright Fachhochschule Duesseldorf, 2012
- * @link http://www.fh-duesseldorf.de
- * @author Fabian Martinovic (FM), <fabian.martinovic@fh-duesseldorf.de>
- */
-
-class LoginController{
-    
-    /**
-     * Konstruktor des Login-Controllers
-     * @param Object $Data
-     */
-    public function __construct(){
-        // Login-Modell einbinden
-        require_once '/models/login.php';
-        // und Objekt erstellen
-        new Login();
-    }
-}
-
- 
-/* End of file lognController.php */
-=======
 <?php
 
 /**
@@ -58,5 +29,4 @@ class LoginController{
 
  
 /* End of file lognController.php */
->>>>>>> origin/mensa
 /* Location: ./controllers/loginController.php */
