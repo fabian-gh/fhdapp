a4a88301c79dba71207110b254361178fb1d2dee
Sascha M <enailil,de@ads.ed>
2013-01-03 00:25:08 +0100

    sicherung

---
+++ views/veranstaltungen/backend_eintraege.php
@@ -0,0 +1,71 @@
+<?PHP
+		
+		//require_once '../../controllers/veranstaltungenController.php';
+		//$Controller = new VeranstaltungenController();
+		//$ergebnis =  $Controller->getInformation(1,1);	
+		 
+		require_once 'backend_dummy_formular.php';
+				 
+		$jquery_complete = ' ';
+		$jquery_dummy = '
+						$("#veranstaltung_anzeigen_###ID###").click(function(){
+								$("#edit_veranstaltung_###ID###").hide();
+								$("#show_veranstaltung_###ID###").slideToggle("fast")();
+							});
+									
+							
+						$("#veranstaltung_bearbeiten_###ID###").click(function(){
+								$("#show_veranstaltung_###ID###").hide();
+								$("#edit_veranstaltung_###ID###").slideToggle("fast")();
+							});
+						';
+		
+		//foreach($ergebnis as $details)
+		//{
+		for($i = 0 ;$i < 5;$i++)
+		{
+			$dummy = $dummy1;
+			
+			$NAME  			= 'TEST';
+			$ID    			=  $i;
+			$TAG   			= '17';
+			$MONAT 			= '06';
+			$JAHR 			= '2012';
+			$BESCHREIBUNG	= 'MEIN GEBURTSTAG';
+			$FB1			= '  ';  	
+			$FB2			= '  ';  	
+			$FB3			= '  ';  	
+			$FB4			= '  ';  	
+			$FB5			= '  ';  	
+			$FB6			= '  ';  	
+			$FB7			= 'X';  	
+			$INTERESSENT	= '  ';	
+			$STUDENT		= '  ';	
+			$ERSTI			= '  ';	
+			
+			
+			$dummy =  str_replace ('###NAME###'			,$NAME  			,$dummy );
+			$dummy =  str_replace ('###ID###'			,$ID    			,$dummy );
+			$dummy =  str_replace ('###TAG###'			,$TAG   			,$dummy );
+			$dummy =  str_replace ('###MONAT###'		,$MONAT 			,$dummy );
+			$dummy =  str_replace ('###JAHR###'			,$JAHR 				,$dummy );
+			$dummy =  str_replace ('###BESCHREIBUNG###'	,$BESCHREIBUNG		,$dummy );
+			$dummy =  str_replace ('###FB1###'			,$FB1		  		,$dummy );
+			$dummy =  str_replace ('###FB2###'			,$FB2		  		,$dummy );
+			$dummy =  str_replace ('###FB3###'			,$FB3		  		,$dummy );
+			$dummy =  str_replace ('###FB4###'			,$FB4		  		,$dummy );
+			$dummy =  str_replace ('###FB5###'			,$FB5		  		,$dummy );
+			$dummy =  str_replace ('###FB6###'			,$FB6		  		,$dummy );
+			$dummy =  str_replace ('###FB7###'			,$FB7		  		,$dummy );
+			$dummy =  str_replace ('###INTERESSENT###'	,$INTERESSENT		,$dummy );
+			$dummy =  str_replace ('###STUDENT###'		,$STUDENT			,$dummy );
+			$dummy =  str_replace ('###ERSTI###'		,$ERSTI				,$dummy );
+			
+			echo $dummy;
+			echo '<br><br><br><br>';
+			
+			$jquery_complete = $jquery_complete . str_replace ('###ID###'	,$ID	,$jquery_dummy );
+			
+		}
+		//}
+?>
---
+++ views/veranstaltungen/backend_formular.php
@@ -0,0 +1,85 @@
+<?PHP
+		echo'
+		<div class="veranstaltung_new" style="border-width:1px; border-style:solid;">
+		
+		<input class="new_formular_button" type="button"  value="Neuen Eintrag erstellen"> 
+		<div class="new_formular" style="display:none;">
+		<form action="?id=backend_veranstaltungen" method="post">
+		
+		<table id="table_veranstaltung_backend" border="0">
+			<thead>
+			<tr>
+			  <th colspan="2">Veranstaltung erstellen</th>
+			</tr>
+		   </thead>
+			<tfoot>
+			<tr>
+			  <td colspan="2">
+				<input type="submit" name="veranstaltung_speichern" id="veranstaltung_speichern" value="Speichern">
+				<input type="hidden" name="veranstaltung_speichern" id="veranstaltung_speichern" value="_new">
+			  </td>
+			</tr>
+		  </tfoot>
+		  
+		  <tbody>
+		  
+			<tr>
+			  <td>Name:</td>
+			  <td>
+				<div data-role="fieldcontain" class="ui-hide-label">
+					<input type="text" name="veranstaltung_name" id="veranstaltung_name" value="" placeholder="Name" size="50" maxlength="30" />
+				</div>
+			  </td>
+			</tr>
+			
+			<tr>
+			 <td>Datum:</td>
+			  <td>
+			  <div data-role="fieldcontain" class="ui-hide-label">
+					<input type="text" name="veranstaltung_datum_tag" id="veranstaltung_datum_tag" value="" placeholder="DD" size="5" maxlength="2" />
+					<input type="text" name="veranstaltung_datum_monat" id="veranstaltung_datum_monat" value="" placeholder="MM" size="5" maxlength="2" />
+					<input type="text" name="veranstaltung_datum_jahr" id="veranstaltung_datum_jahr" value="" placeholder="YYYY" size="10" maxlength="4" />
+			  </div>
+			  </td>
+			</tr>
+			
+			<tr>
+			  <td>Beschreibung:</td>
+			  <td>
+				<div data-role="fieldcontain" class="ui-hide-label">
+				<textarea name="veranstaltung_beschreibung" id="veranstaltung_beschreibung" cols="50" rows="10"></textarea>
+				</div>
+			  </td>
+			</tr>
+			
+			<tr>
+			  <td>Fachbereich:</td>
+			  <td>	
+			  
+					<label><input type="checkbox" name="veranstaltungen_fachbereich_1" /> Fachbereich 1 - Architektur  </label>
+					<label><input type="checkbox" name="veranstaltungen_fachbereich_2" /> Fachbereich 2 - Design </label>
+					<label><input type="checkbox" name="veranstaltungen_fachbereich_3" /> Fachbereich 3 - Elektrotechnik </label>
+					<label><input type="checkbox" name="veranstaltungen_fachbereich_4" /> Fachbereich 4 - Maschinenbau und Verfahrenstechnik </label>
+					<label><input type="checkbox" name="veranstaltungen_fachbereich_5" /> Fachbereich 5 - Medien </label>
+					<label><input type="checkbox" name="veranstaltungen_fachbereich_6" /> Fachbereich 6 - Sozial- und Kulturwissenschaften </label>
+					<label><input type="checkbox" name="veranstaltungen_fachbereich_7" /> Fachbereich 7 - Wirtschaft </label>
+			  </td>
+			</tr>
+			
+			<tr>
+			  <td>Modus:</td>
+			  <td>
+					<label><input type="checkbox" name="veranstaltungen_usertypes_1" /> Interessent </label>
+					<label><input type="checkbox" name="veranstaltungen_usertypes_2" /> Ersti </label>
+					<label><input type="checkbox" name="veranstaltungen_usertypes_3" /> Student </label>
+			  </td>
+			</tr>
+			
+		  </tbody>
+		</table>
+		
+		</form>
+		</div>
+		</div>
+		';
+?>
