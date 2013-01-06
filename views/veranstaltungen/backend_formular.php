b4ecb92ed317ffaa7a1a8fa700dc2c662a18e935
Sascha M <enailil,de@ads.ed>
2013-01-06 01:42:55 +0100

    Formular-Klasse
    Klasse die alles was mit Formularen zusammenhängt regelt.

---
+++ views/veranstaltungen/backend_formular.php
@@ -0,0 +1,478 @@
+<?php
+
+/**
+ * FHD-App
+ * @version 0.0.1
+ * @copyright Fachhochschule Duesseldorf, 2012
+ * @link http://www.fh-duesseldorf.de
+ * @author Sascha Möller (FM), <sascha.moeller@fh-duesseldorf.de>
+ */
+ 
+class Formular{
+
+	private $NAME = 		null;
+	private $ID = 			null;
+	private $TAG = 			null;
+	private $MONAT = 		null;
+	private $JAHR = 		null;
+	private $STUNDEN = 		null;
+	private $MINUTEN = 		null;
+	private $BESCHREIBUNG = null;
+	private $FB1 = 			null;
+	private $FB2 = 			null;
+	private $FB3 = 			null;
+	private $FB4 = 			null;
+	private $FB5 = 			null;
+	private $FB6 = 			null;
+	private $FB7 = 			null;
+	private $INTERESSENT =	null;
+	private $STUDENT =	 	null;
+	private $ERSTI = 		null;
+	
+
+	public function __construct(){
+        // Konstruktor
+    }
+	
+	public function getEmptyForm()
+	{
+		//Alle Variablen auf null setzen bis auf die ID
+		$this->setALL(null, 'new', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
+		$this->replaceALL();
+		
+		$RESULT =
+		'
+		<div class="veranstaltung_'.$this->ID.'" style="border-width:1px; border-style:solid;">
+		
+		<a class="button" id="veranstaltung_anzeigen_'.$this->ID.'">Neuen Eintrag erstellen</a>
+		';
+		
+		$RESULT .= $this->EVENTFORM;
+		$RESULT .= '</div>';
+		return $RESULT;
+	}
+	
+	public function getEventContainer($FB_GET)
+	{
+		$RESULT =
+		'
+		<div class="veranstaltung_'.$this->ID.'" style="border-width:1px; border-style:solid;">
+		<h3>'.$this->NAME.'</h3>
+		
+		<a class="button" id="veranstaltung_anzeigen_'.$this->ID.'">Veranstaltung anzeigen</a>
+		<a class="button" id="veranstaltung_bearbeiten_'.$this->ID.'">Veranstaltung bearbeiten</a>
+		<a href="?FB='.$FB_GET.'&amp;loeschen='.$this->ID.'" class="button" id="loesch_button">L&ouml;schen</a>
+		';
+		
+		$RESULT .= $this->getEventResult();
+		$RESULT .= $this->getEventForm();
+		
+		$RESULT .= '</div>';
+		return $RESULT;
+	}
+	
+	public function getEventForm()
+	{
+		return $this->EVENTFORM;	
+	}
+	
+	public function getEventResult()
+	{
+		return $this->EVENTRESULT;
+	}
+	
+	public function getJquery()
+	{
+		$JQUERY = 
+				'
+				$("#veranstaltung_anzeigen_'.$this->ID.'").click(function(){
+					$(".form_veranstaltung_new").hide();
+					$("#form_veranstaltung_'.$this->ID.'").hide();
+					$("#show_veranstaltung_'.$this->ID.'").slideToggle("fast");
+				});
+	
+				$("#veranstaltung_bearbeiten_'.$this->ID.'").click(function(){
+					$(".form_veranstaltung_new").hide();
+					$("#show_veranstaltung_'.$this->ID.'").hide();
+					$("#form_veranstaltung_'.$this->ID.'").slideToggle("fast");
+				});
+				';
+		return $JQUERY;
+	}
+	
+	public function getJqueryEmptyForm()
+	{
+		$JQUERY = 
+				'
+				$("#veranstaltung_anzeigen_'.$this->ID.'").click(function(){
+						$("#form_veranstaltung_'.$this->ID.'").slideToggle("fast");
+				});
+				';
+		return $JQUERY;
+	}
+	
+	public function setALL($NAME, $ID, $TAG, $MONAT, $JAHR, $STUNDEN, $MINUTEN, $BESCHREIBUNG, $FB1, $FB2, $FB3, $FB4, $FB5, $FB6, $FB7, $INTERESSENT, $STUDENT, $ERSTI)
+	{
+		$this->NAME = $NAME;
+		$this->ID = $ID;
+		$this->TAG = $TAG;
+		$this->MONAT = $MONAT;
+		$this->JAHR = $JAHR;
+		$this->STUNDEN = $STUNDEN;
+		$this->MINUTEN = $MINUTEN;
+		$this->BESCHREIBUNG = $BESCHREIBUNG;
+		$this->FB1 = $FB1;
+		$this->FB2 = $FB2;
+		$this->FB3 = $FB3;
+		$this->FB4 = $FB4;
+		$this->FB5 = $FB5;
+		$this->FB6 = $FB6;
+		$this->FB7 = $FB7;
+		$this->INTERESSENT = $INTERESSENT;
+		$this->STUDENT = $STUDENT;
+		$this->ERSTI = $ERSTI;
+		
+		$this->replaceALL();
+	}	
+
+	public function replaceALL()
+	{	
+		$this->EVENTFORM =  str_replace ('###NAME###'				,$this->NAME  			,$this->EVENTFORM );
+		$this->EVENTFORM =  str_replace ('###ID###'					,$this->ID    			,$this->EVENTFORM );
+		$this->EVENTFORM =  str_replace ('###TAG###'				,$this->TAG   			,$this->EVENTFORM );
+		$this->EVENTFORM =  str_replace ('###MONAT###'				,$this->MONAT 			,$this->EVENTFORM );
+		$this->EVENTFORM =  str_replace ('###JAHR###'				,$this->JAHR 			,$this->EVENTFORM );
+		$this->EVENTFORM =  str_replace ('###STUNDEN###'			,$this->STUNDEN			,$this->EVENTFORM );
+		$this->EVENTFORM =  str_replace ('###MINUTEN###'			,$this->MINUTEN			,$this->EVENTFORM );
+		$this->EVENTFORM =  str_replace ('###BESCHREIBUNG###'		,$this->BESCHREIBUNG	,$this->EVENTFORM );
+		$this->EVENTFORM =  str_replace ('###FB1###'				,$this->FB1		  		,$this->EVENTFORM );
+		$this->EVENTFORM =  str_replace ('###FB2###'				,$this->FB2		  		,$this->EVENTFORM );
+		$this->EVENTFORM =  str_replace ('###FB3###'				,$this->FB3		  		,$this->EVENTFORM );
+		$this->EVENTFORM =  str_replace ('###FB4###'				,$this->FB4		  		,$this->EVENTFORM );
+		$this->EVENTFORM =  str_replace ('###FB5###'				,$this->FB5		  		,$this->EVENTFORM );
+		$this->EVENTFORM =  str_replace ('###FB6###'				,$this->FB6		  		,$this->EVENTFORM );
+		$this->EVENTFORM =  str_replace ('###FB7###'				,$this->FB7		  		,$this->EVENTFORM );
+		$this->EVENTFORM =  str_replace ('###INTERESSENT###'		,$this->INTERESSENT		,$this->EVENTFORM );
+		$this->EVENTFORM =  str_replace ('###STUDENT###'			,$this->STUDENT			,$this->EVENTFORM );
+		$this->EVENTFORM =  str_replace ('###ERSTI###'				,$this->ERSTI			,$this->EVENTFORM );
+		
+		$this->EVENTRESULT =  str_replace ('###NAME###'				,$this->NAME  			,$this->EVENTRESULT );
+		$this->EVENTRESULT =  str_replace ('###ID###'				,$this->ID    			,$this->EVENTRESULT );
+		$this->EVENTRESULT =  str_replace ('###TAG###'				,$this->TAG   			,$this->EVENTRESULT );
+		$this->EVENTRESULT =  str_replace ('###MONAT###'			,$this->MONAT 			,$this->EVENTRESULT );
+		$this->EVENTRESULT =  str_replace ('###JAHR###'				,$this->JAHR 			,$this->EVENTRESULT );
+		$this->EVENTRESULT =  str_replace ('###STUNDEN###'			,$this->STUNDEN			,$this->EVENTRESULT );
+		$this->EVENTRESULT =  str_replace ('###MINUTEN###'			,$this->MINUTEN			,$this->EVENTRESULT );
+		$this->EVENTRESULT =  str_replace ('###BESCHREIBUNG###'		,$this->BESCHREIBUNG	,$this->EVENTRESULT );
+		$this->EVENTRESULT =  str_replace ('###FB1###'				,$this->FB1		  		,$this->EVENTRESULT );
+		$this->EVENTRESULT =  str_replace ('###FB2###'				,$this->FB2		  		,$this->EVENTRESULT );
+		$this->EVENTRESULT =  str_replace ('###FB3###'				,$this->FB3		  		,$this->EVENTRESULT );
+		$this->EVENTRESULT =  str_replace ('###FB4###'				,$this->FB4		  		,$this->EVENTRESULT );
+		$this->EVENTRESULT =  str_replace ('###FB5###'				,$this->FB5		  		,$this->EVENTRESULT );
+		$this->EVENTRESULT =  str_replace ('###FB6###'				,$this->FB6		  		,$this->EVENTRESULT );
+		$this->EVENTRESULT =  str_replace ('###FB7###'				,$this->FB7		  		,$this->EVENTRESULT );
+		$this->EVENTRESULT =  str_replace ('###INTERESSENT###'		,$this->INTERESSENT		,$this->EVENTRESULT );
+		$this->EVENTRESULT =  str_replace ('###STUDENT###'			,$this->STUDENT			,$this->EVENTRESULT );
+		$this->EVENTRESULT =  str_replace ('###ERSTI###'			,$this->ERSTI			,$this->EVENTRESULT );
+	}
+	
+	public function getNAME(){
+		return $this->NAME;
+	}
+
+	public function setNAME($NAME){
+		$this->NAME = $NAME;
+	}
+
+	public function getID(){
+		return $this->ID;
+	}
+
+	public function setID($ID){
+		$this->ID = $ID;
+	}
+
+	public function getTAG(){
+		return $this->TAG;
+	}
+
+	public function setTAG($TAG){
+		$this->TAG = $TAG;
+	}
+
+	public function getMONAT(){
+		return $this->MONAT;
+	}
+
+	public function setMONAT($MONAT){
+		$this->MONAT = $MONAT;
+	}
+
+	public function getJAHR(){
+		return $this->JAHR;
+	}
+
+	public function setJAHR($JAHR){
+		$this->JAHR = $JAHR;
+	}
+
+	public function getSTUNDEN(){
+		return $this->STUNDEN;
+	}
+
+	public function setSTUNDEN($STUNDEN){
+		$this->STUNDEN = $STUNDEN;
+	}
+
+	public function getMINUTEN(){
+		return $this->MINUTEN;
+	}
+
+	public function setMINUTEN($MINUTEN){
+		$this->MINUTEN = $MINUTEN;
+	}
+
+	public function getBESCHREIBUNG(){
+		return $this->BESCHREIBUNG;
+	}
+
+	public function setBESCHREIBUNG($BESCHREIBUNG){
+		$this->BESCHREIBUNG = $BESCHREIBUNG;
+	}
+
+	public function getFB1(){
+		return $this->FB1;
+	}
+
+	public function setFB1($FB1){
+		$this->FB1 = $FB1;
+	}
+
+	public function getFB2(){
+		return $this->FB2;
+	}
+
+	public function setFB2($FB2){
+		$this->FB2 = $FB2;
+	}
+
+	public function getFB3(){
+		return $this->FB3;
+	}
+
+	public function setFB3($FB3){
+		$this->FB3 = $FB3;
+	}
+
+	public function getFB4(){
+		return $this->FB4;
+	}
+
+	public function setFB4($FB4){
+		$this->FB4 = $FB4;
+	}
+
+	public function getFB5(){
+		return $this->FB5;
+	}
+
+	public function setFB5($FB5){
+		$this->FB5 = $FB5;
+	}
+
+	public function getFB6(){
+		return $this->FB6;
+	}
+
+	public function setFB6($FB6){
+		$this->FB6 = $FB6;
+	}
+
+	public function getFB7(){
+		return $this->FB7;
+	}
+
+	public function setFB7($FB7){
+		$this->FB7 = $FB7;
+	}
+
+	public function getINTERESSENT(){
+		return $this->INTERESSENT;
+	}
+
+	public function setINTERESSENT($INTERESSENT){
+		$this->INTERESSENT = $INTERESSENT;
+	}
+
+	public function getSTUDENT(){
+		return $this->STUDENT;
+	}
+
+	public function setSTUDENT($STUDENT){
+		$this->STUDENT = $STUDENT;
+	}
+
+	public function getERSTI(){
+		return $this->ERSTI;
+	}
+
+	public function setERSTI($ERSTI){
+		$this->ERSTI = $ERSTI;
+	}
+	
+	private $EVENTFORM =
+	'<div class="veranstaltung" id="form_veranstaltung_###ID###" style="display:none;">
+		
+			<form action="" method="post">
+			<table id="table_veranstaltung_backend" border="0">
+				<thead>
+				<tr>
+				  <th colspan="2">Veranstaltung Bearbeiten</th>
+				</tr>
+			   </thead>
+				<tfoot>
+				<tr>
+				  <td colspan="2">
+					<input type="submit" name="veranstaltung_speichern" id="veranstaltung_speichern" value="Speichern">
+					<input type="hidden" name="veranstaltung_id" id="veranstaltung_id" value="###ID###">
+				  </td>
+				</tr>
+			  </tfoot>
+			  
+			  <tbody>
+			  
+				<tr>
+				  <td>Name:</td>
+				  <td>
+					<div data-role="fieldcontain" class="ui-hide-label">
+						<input type="text" name="veranstaltung_name" id="veranstaltung_name" value="###NAME###" placeholder="Name" size="50" maxlength="30" />
+					</div>
+				  </td>
+				</tr>
+				
+				<tr>
+				 <td>Datum:</td>
+				  <td>
+				  <div data-role="fieldcontain" class="ui-hide-label">
+						<input type="text" name="veranstaltung_datum_tag" id="veranstaltung_datum_tag" value="###TAG###" placeholder="DD" size="5" maxlength="2" />
+						<input type="text" name="veranstaltung_datum_monat" id="veranstaltung_datum_monat" value="###MONAT###" placeholder="MM" size="5" maxlength="2" />
+						<input type="text" name="veranstaltung_datum_jahr" id="veranstaltung_datum_jahr" value="###JAHR###" placeholder="YYYY" size="10" maxlength="4" />
+				  </div>
+				  </td>
+				</tr>
+				
+				<tr>
+				<td>Uhrzeit:</td>
+				<td>
+					<div data-role="fieldcontain" class="ui-hide-label">
+						<input type="text" name="veranstaltung_uhrzeit_stunden" id="veranstaltung_uhrzeit_stunden" value="###STUNDEN###" placeholder="HH" size="5" maxlength="2" />
+						<input type="text" name="veranstaltung_uhrzeit_minuten" id="veranstaltung_uhrzeit_minuten" value="###MINUTEN###" placeholder="MM" size="5" maxlength="2" />
+					</div>
+				</td>
+				</tr>
+				
+				<tr>
+				  <td>Beschreibung:</td>
+				  <td>
+					<div data-role="fieldcontain" class="ui-hide-label">
+					<textarea name="veranstaltung_beschreibung" id="veranstaltung_beschreibung" cols="50" rows="10">###BESCHREIBUNG###</textarea>
+					</div>
+				  </td>
+				</tr>
+				
+				<tr>
+				  <td>Fachbereich:</td>
+				  <td>	
+				  
+						<label><input type="checkbox" name="veranstaltungen_fachbereich_1" 	   ###FB1###/> Fachbereich 1 - Architektur  </label>
+						<br><label><input type="checkbox" name="veranstaltungen_fachbereich_2" ###FB2###/> Fachbereich 2 - Design </label>
+						<br><label><input type="checkbox" name="veranstaltungen_fachbereich_3" ###FB3###/> Fachbereich 3 - Elektrotechnik </label>
+						<br><label><input type="checkbox" name="veranstaltungen_fachbereich_4" ###FB4###/> Fachbereich 4 - Maschinenbau und Verfahrenstechnik </label>
+						<br><label><input type="checkbox" name="veranstaltungen_fachbereich_5" ###FB5###/> Fachbereich 5 - Medien </label>
+						<br><label><input type="checkbox" name="veranstaltungen_fachbereich_6" ###FB6###/> Fachbereich 6 - Sozial- und Kulturwissenschaften </label>
+						<br><label><input type="checkbox" name="veranstaltungen_fachbereich_7" ###FB7###/> Fachbereich 7 - Wirtschaft </label>
+				  </td>
+				</tr>
+				
+				<tr>
+				  <td>Modus:</td>
+				  <td>
+						<label><input type="checkbox" name="veranstaltungen_usertypes_1" 	 ###INTERESSENT###/> Interessent </label>
+						<br><label><input type="checkbox" name="veranstaltungen_usertypes_2" ###ERSTI###/> Ersti </label>
+						<br><label><input type="checkbox" name="veranstaltungen_usertypes_3" ###STUDENT###/> Student </label>
+				  </td> 
+				</tr>   
+						
+			  </tbody>  
+			</table>
+			</form>
+		</div>
+	';
+	
+	private $EVENTRESULT =
+	'<div class="veranstaltung" id="show_veranstaltung_###ID###" style="display:none;">
+			<table id="table_veranstaltung_backend" border="0">
+				<thead>
+				</thead>
+				<tfoot>
+				<tr>
+				  <td colspan="2">
+					<input type="hidden" name="veranstaltung_speichern" id="veranstaltung_speichern" value="###ID###">
+				  </td>
+				</tr>
+			  </tfoot>
+			  
+			  <tbody>				
+				<tr>
+				 <td>Datum:</td>
+				  <td>
+					<div class="datum">###TAG###.###MONAT###.###JAHR###</div>
+				  </td>
+				</tr>
+				
+				<tr>
+				 <td>Uhrzeit:</td>
+				  <td>
+					<div class="datum">###STUNDEN###:###MINUTEN###</div>
+				  </td>
+				</tr>
+				
+				<tr>
+				  <td>Beschreibung:</td>
+				  <td>
+					<div class="datum">###BESCHREIBUNG###</div>
+				  </td>
+				</tr>
+				
+				<tr>
+				  <td>Fachbereich:</td>
+				  <td>	
+				  
+						[###FB1###]	Fachbereich 1 - Architektur  						<br>
+						[###FB2###]	Fachbereich 2 - Design 								<br>
+						[###FB3###]	Fachbereich 3 - Elektrotechnik 						<br>
+						[###FB4###]	Fachbereich 4 - Maschinenbau und Verfahrenstechnik 	<br>
+						[###FB5###]	Fachbereich 5 - Medien 								<br>
+						[###FB6###]	Fachbereich 6 - Sozial- und Kulturwissenschaften 	<br>
+						[###FB7###]	Fachbereich 7 - Wirtschaft 							<br>
+				  </td>
+				</tr>
+				
+				
+				<tr>
+				  <td>Modus:</td>
+				  <td>
+						[###INTERESSENT###]	INTERESSENT	<br>
+						[###ERSTI###]		ERSTI		<br>
+						[###STUDENT###]		STUDENT		<br>
+				  </td>
+				</tr>  
+						
+			  </tbody> 
+			</table>
+		</div>
+	';
+}
+ 
+/* End of file backend_formular.php */
+/* Location: ./views/backend_formular.php */
--- views/veranstaltungen/backend_veranstaltungen.php
+++ views/veranstaltungen/backend_veranstaltungen.php
@@ -16,6 +16,9 @@
 	//Conroller einbinden
 	require_once '../../controllers/veranstaltungenController.php';
 	$Controller = new VeranstaltungenController;
+	
+	//Klasse für Formularfelder einbinden
+	require_once 'backend_formular.php';
 		
 	//Überprüfung ob Formular abgesendet wurde
 	if(isset($_POST['veranstaltung_speichern']))
@@ -23,53 +26,138 @@
 		echo 'Vielen Dank';
 		$Controller->addDatensatz();
 	}
-	//Falls kein Formular übergeben wurde, dann leeres Formular anzeigen
 	else
 	{
 		if(isset($_GET['FB']))
 			$FB_GET = $_GET['FB'];
 		else
 			$FB_GET = 1;
-			
-		require_once 'backend_formular_neuer_Eintrag.php';
-		echo '<br><br><br><br>';
-				
-		require_once 'backend_fachbereich_auswahl.php';
-		echo '<br><br><br><br>';
 		
-		require_once 'backend_datenbank_eintraege_control.php';
-		echo '<br><br><br><br>';
+		//Variable für alle JQuery Methoden, wird am Ende des Dokuments ausgegeben
+		$JQUERY = '';
+		
+		//Neues Objekt von Formular erstellen
+		$Formular = new Formular;
+		//Leeres Formular erstellen
+		echo $Formular->getEmptyForm();
+		//JQuery für das leere Formular erstellen
+		$JQUERY .= $Formular->getJqueryEmptyForm();
+		
+		echo '<br/><br/><br/><br/>';
+		
+		//Auswahl des Fachbereiches 
+		//Leeres Array mit 8 Feldern
+		$SELECTED = array('', '', '' ,'', '', '', '');
+		$SELECTED[$FB_GET-1] = 'Selected';
+		echo'
+			<div id="div_fachbereich_auswahl">
+				<h3>W&auml;hlen Sie den Fachbereich aus f&uuml;r den Sie die Veranstaltungen bearbeiten m&ouml;chten</h3>
+				<form id="fachbereich_auswahl" action="">
+					<select id="fachbereich_select" name="FB" size="1">
+						<option value="1" '.$SELECTED[1-1].'> Fachbereich 1 - Architektur  </option>
+						<option value="2" '.$SELECTED[2-1].'> Fachbereich 2 - Design </option>
+						<option value="3" '.$SELECTED[3-1].'> Fachbereich 3 - Elektrotechnik </option>
+						<option value="4" '.$SELECTED[4-1].'> Fachbereich 4 - Maschinenbau und Verfahrenstechnik </option>
+						<option value="5" '.$SELECTED[5-1].'> Fachbereich 5 - Medien </option>
+						<option value="6" '.$SELECTED[6-1].'> Fachbereich 6 - Sozial- und Kulturwissenschaften </option>
+						<option value="7" '.$SELECTED[7-1].'> Fachbereich 7 - Wirtschaft </option>
+					</select>
+				</form>
+			</div>
+		';
+		
+		echo '<br/><br/><br/><br/>';
+		
+		//Datenbank-Abfrage alle Veranstaltungen für aktuellen Fachbereich laden
+		$ERGEBNIS =  $Controller->getInformationEventsWithDepartmentsWihoutUsertype($FB_GET);
+		
+		if($ERGEBNIS != null)
+		{
+			//Veranstaltungen durchlaufen und darstellen
+			for($i=0; $i<count($ERGEBNIS); $i++) 
+			{
+				$NAME = $ERGEBNIS[$i]['name'];
+				$ID = $ERGEBNIS[$i]['id'];
+				$BESCHREIBUNG = $ERGEBNIS[$i]['description'];	
+				$DATUM = new DateTime($ERGEBNIS[$i]['date']);		
+				
+				//DATUM SPLITTEN
+				$TAG = 		date_format($DATUM, 'd');
+				$MONAT = 	date_format($DATUM, 'm');
+				$JAHR = 	date_format($DATUM, 'Y');
+				$STUNDEN = 	date_format($DATUM, 'H');
+				$MINUTEN =	date_format($DATUM, 'i');
+				
+				$FB1			= '  ';  	
+				$FB2			= '  ';  	
+				$FB3			= '  ';  	
+				$FB4			= '  ';  	
+				$FB5			= '  ';  	
+				$FB6			= '  ';  	
+				$FB7			= 'X ';  	
+				$INTERESSENT	= '  ';	
+				$STUDENT		= '  ';	
+				$ERSTI			= '  ';	
+				
+				//Neues Objekt von Formular erstellen
+				$Formular = new Formular;
+				//Alle Variablen setzen
+				$Formular->setALL(
+								$NAME, 
+								$ID, 
+								$TAG, 
+								$MONAT, 
+								$JAHR, 
+								$STUNDEN, 
+								$MINUTEN, 
+								$BESCHREIBUNG, 
+								$FB1, 
+								$FB2, 
+								$FB3, 
+								$FB4, 
+								$FB5, 
+								$FB6, 
+								$FB7, 
+								$INTERESSENT,
+								$STUDENT, 
+								$ERSTI
+								);
+				//Veranstaltung darstellen mit Bearbeiten-Option
+				echo $Formular->getEventContainer($FB_GET);
+				//JQuery erstellen
+				$JQUERY .= $Formular->getJquery();
+			}
+		}
+		else
+		{
+			echo "Kein Datensatz vorhanden!";
+		}
+				
+		//require_once 'backend_datenbank_eintraege_control.php';
+		echo '<br/><br/><br/><br/>';
 		
 		echo '
-		<script type="text/javascript">
-			$(function(){ 
-				$("#new_formular_button").click(function(){
-					hide_all();
-					$("#new_formular").slideToggle("fast");
-				});
-				
-				$("#fachbereich_select").change(function(){
-					$("#fachbereich_auswahl").submit();
-				});
-				
-				$("#loesch_button").click(function(){
-					$("#loesch_form").submit();
-				});
-				
-				function hide_all()
-				{
-					$(".edit_veranstaltung").hide();
-					$(".show_veranstaltung").hide();
-				}
+			<script type="text/javascript">
+					$(function(){
 					
-				'.$jquery_complete.'
-			});
-		</script>
+					$("#fachbereich_select").change(function(){
+						$("#fachbereich_auswahl").submit();
+					});
+					
+					'.$JQUERY.'	
+				});
+			</script>
 		';
 	}
 	
 	require_once '../../layout/backend/footer.php';
 	ob_end_flush();
 	/* End of file veranstaltungen_edit.php */
+	/*
+					$("#new_formular_button").click(function(){
+						hide_all();
+						$("#new_formular").slideToggle("fast");
+					});
+	*/
 	/* Location: ./views/veranstaltungen/veranstaltungen_edit.php */
 ?>
