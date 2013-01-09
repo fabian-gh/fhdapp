c4fd72e6cac6145a10be21a2b8a98c8fe722adee
Fabian Martinovic <jimmy_crowbar@gmx.de>
2013-01-09 17:02:06 +0100

    neue Dateien mit index

---
+++ views/navigation/start.php
@@ -0,0 +1,3 @@
+<a href='index.php?eis=i'>Interessent</a><br>
+<a href='index.php?eis=e'>Ersti</a><br>
+<a href='index.php?eis=s'>Student</a><br>
---
+++ views/navigation/subcategory.php
@@ -0,0 +1,15 @@
+<?php
+
+	if($_GET['eis'] == 'i')
+		echo "hier stehen alle unterkategorien von interessenten<br>
+		<a href='index.php?eis={$_GET['eis']}&selector={$_GET['selector']}&course={$_GET['course']}&page=Termine'>Termine</a>";
+
+	else if($_GET['eis'] == 'e')
+		echo "hier stehen alle unterkategorien von erstis
+		<a href='index.php?eis={$_GET['eis']}&selector={$_GET['selector']}&course={$_GET['course']}&page=Termine'>Termine</a>";
+
+	else if($_GET['eis'] == 's')
+		echo "hier stehen alle unterkategorien von studenten
+		<a href='index.php?eis={$_GET['eis']}&selector={$_GET['selector']}&course={$_GET['course']}&page=Termine'>Termine</a>";
+
+?>
