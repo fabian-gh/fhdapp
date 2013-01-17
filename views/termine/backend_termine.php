21242529e99b1eb1e8e909d8fcb19ff36433ecc8
mrgrimmig <dkirchhof@gmail.com>
2013-01-17 22:24:55 +0100

    focus bekommen damit onblur funktionier ->chrome + confirm-text geändert

--- views/termine/backend_termine.php
+++ views/termine/backend_termine.php
@@ -83,7 +83,7 @@
             
             <form action='' method='post'>
             <input type='hidden' name='id' value='$id'/>
-            <tr><td>$id</td><td><input type='text' name='from' value='$from' onblur=\"checkYear(this);\"/></td><td colspan='5'><input type='radio' name='type' value='summer' $summer/> Sommer <input type='radio' name='type' value='winter' $winter/> Winter</td><td><input name='saveSemester' type='submit' value='Speichern' onclick=\"return confirm('Möchten Sie die Änderungen speichern?')\" /><input name='removeSemester' type='submit' value='Entfernen' onclick=\"return confirm('Möchten Sie das Semester und alle zugehörigen Termine entfernen?')\"/></td></tr>
+            <tr><td>$id</td><td><input type='text' name='from' value='$from' onclick='this.focus()' onblur=\"checkYear(this);\"/></td><td colspan='5'><input type='radio' name='type' value='summer' $summer/> Sommer <input type='radio' name='type' value='winter' $winter/> Winter</td><td><input name='saveSemester' type='submit' value='Speichern' onclick=\"return confirm('Die Änderungen des Semesters mit der ID $id werden gespeichert.')\" /><input name='removeSemester' type='submit' value='Entfernen' onclick=\"return confirm('Das Semester mit der ID $id und alle zugehörigen Termine werden entfernt.')\"/></td></tr>
             </form>";
         
             //termine in block einfügen
@@ -101,14 +101,14 @@
                     echo "<form action='' method='post'>
                         <input type='hidden' name='appointment' value='{$appointment['id']}'/>
                         <input type='hidden' name='semester' value='$id'/>
-                        <tr><td>{$appointment['id']}</td><td><input type='text' name='name' value='{$appointment['name']}' onblur=\"checkName(this);\"/></td><td><input type='text' name='date_from' value='$date_from' onblur=\"checkDate(this);\"/></td><td><input type='text' name='date_to' value='$date_to' onblur=\"checkDate(this);\"/></td><td><input type='checkbox' name='interested' value='true' $interested /></td><td><input type='checkbox' name='freshman' value='true' $freshman /></td><td><input type='checkbox' name='student' value='true' $student /></td><td><input name='saveAppointment' type='submit' value='Speichern' onclick=\"return confirm('Möchten Sie die Änderungen speichern?')\"/><input name='removeAppointment' type='submit' value='Entfernen' onclick=\"return confirm('Möchten Sie den Termin entfernen?')\"/></td></tr>
+                        <tr><td>{$appointment['id']}</td><td><input type='text' name='name' value='{$appointment['name']}' onclick='this.focus()' onblur=\"checkName(this);\"/></td><td><input type='text' name='date_from' value='$date_from' onclick='this.focus()' onblur=\"checkDate(this);\"/></td><td><input type='text' name='date_to' value='$date_to' onclick='this.focus()' onblur=\"checkDate(this);\"/></td><td><input type='checkbox' name='interested' value='true' $interested /></td><td><input type='checkbox' name='freshman' value='true' $freshman /></td><td><input type='checkbox' name='student' value='true' $student /></td><td><input name='saveAppointment' type='submit' value='Speichern' onclick=\"return confirm('Die Änderungen im Termin mit der ID {$appointment['id']} werden gespeichert.')\"/><input name='removeAppointment' type='submit' value='Entfernen' onclick=\"return confirm('Der Termin mit der ID {$appointment['id']} wird entfernt.')\"/></td></tr>
                     </form>";
                 }
 
             //form zum termin hinzufügen
             echo "<form action='' method='post'>
                 <input type='hidden' name='semester' value='$id'/>
-                <tr><td></td><td><input type='text' name='name' value='Terminname' onblur=\"checkName(this);\"/></td><td><input type='text' name='date_from' value='T.M.JJJJ' onblur=\"checkDate(this);\"/></td><td><input type='text' name='date_to' value='T.M.JJJJ' onblur=\"checkDate(this);\"/></td><td><input type='checkbox' name='interested' value='true' /></td><td><input type='checkbox' name='freshman' value='true' /></td><td><input type='checkbox' name='student' value='true' /></td><td><input name='saveAppointment' type='submit' value='Hinzufügen'/><input type='reset' value='Felder zurücksetzen'/></td></tr>
+                <tr><td></td><td><input type='text' name='name' value='Terminname' onclick='this.focus()' onblur=\"checkName(this);\"/></td><td><input type='text' name='date_from' value='T.M.JJJJ' onclick='this.focus()' onblur=\"checkDate(this);\"/></td><td><input type='text' name='date_to' value='T.M.JJJJ' onclick='this.focus()' onblur=\"checkDate(this);\"/></td><td><input type='checkbox' name='interested' value='true' /></td><td><input type='checkbox' name='freshman' value='true' /></td><td><input type='checkbox' name='student' value='true' /></td><td><input name='saveAppointment' type='submit' value='Hinzufügen'/><input type='reset' value='Felder zurücksetzen'/></td></tr>
             </form>
         </table>";
     }
@@ -119,7 +119,7 @@
 
             <form action='' method='post'>
             <input type='hidden' name='dept' value='{$_GET['dept']}'/>
-            <tr><td></td><td colspan='5'><input type='text' name='from' value='JJJJ' onblur=\"checkYear(this);\"/></td><td><input type='radio' name='type' value='summer' checked='checked'/> Sommer <input type='radio' name='type' value='winter'/> Winter</td><td><input name='saveSemester' type='submit' value='Hinzufügen'/><input type='reset' value='Felder zurücksetzen'/></td></tr>
+            <tr><td></td><td colspan='5'><input type='text' name='from' value='JJJJ' onclick='this.focus()' onblur=\"checkYear(this);\"/></td><td><input type='radio' name='type' value='summer' checked='checked'/> Sommer <input type='radio' name='type' value='winter'/> Winter</td><td><input name='saveSemester' type='submit' value='Hinzufügen'/><input type='reset' value='Felder zurücksetzen'/></td></tr>
             </form>
         </table>";
 
