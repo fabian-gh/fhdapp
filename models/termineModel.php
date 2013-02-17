347fb93ccef7e1fea5c4b10a74ec89fcfe000fe1
mrgrimmig <dkirchhof@gmail.com>
2013-02-17 20:39:33 +0100

    kommentare hinzugefügt

--- models/termineModel.php
+++ models/termineModel.php
@@ -1,9 +1,15 @@
 <?php
 
+	/**
+	 * Schnittstelle zwischen Controller und Datenbank für Termine
+	**/
 	class AppointmentModel
 	{
 		private $connection;
 
+		/**
+		 * Erstellt das Model und stelt Verbindung zur Datenbank her
+		**/
 		public function __construct()
 		{
 			$this->connection = new mysqli($_SESSION['host'], $_SESSION['user'], $_SESSION['pwd'], $_SESSION['db']);
@@ -12,7 +18,11 @@
 		
 		//semester
 
-		//alle semester eines fachbereichs ausgeben
+		/**
+		 * @param int $dept ID des Fachbereichs
+		 *
+		 * @return array Gibt ein Array mit allen Semestern eines bestimmten Fachbereiches zurück.
+		**/
 		public function getSemesters($dept)
 		{
 			try
@@ -33,7 +43,13 @@
 			}
 		}
 
-		//alle semester für eine bestimmte nutzergruppe ausgeben(nur die semester, welche termine für den bestimmten nutzer haben, also keine leeren)
+		/**
+		 * @param int $dept ID des Fachbereichs
+		 * @param char $eis Buchstabe der Zielgruppe (i = Interessent, e = Erstsemester, s = Student)
+		 *
+		 * @return array Gibt ein Array mit allen Semesterblöcken inklusive ihrer Termine eines bestimmten Fachbereiches und einer bestimmten Zielgruppe zurück.
+		 * Leere Blöcke, bzw. Blöcke, in denen keine treffenden Termine vorhanden sind, werden nicht zurückgegeben.
+		**/
 		public function getSemestersForUsertype($dept, $eis)
 		{
 			try
@@ -46,7 +62,7 @@
 					case 's': $eis = "appointments.student = 1"; break;
 				}
 
-				$result = $this->connection->query("SELECT semester.id, semester.name, count(*)
+				$result = $this->connection->query("SELECT semester.id, semester.name
 														FROM semester, appointments
 														WHERE semester.department_id = $dept && appointments.semester_id = semester.id && $eis
 														GROUP BY semester.id");
@@ -64,7 +80,13 @@
 			}
 		}
 
-		//semester hinzufügen
+		/**
+		 * Fügt ein neues Semester ein
+		 *
+		 * @param int $language ID der Sprache
+		 * @param string $name Name des Semesters
+		 * @param int $department ID des Fachbereichs
+		**/
 		public function insertSemester($language, $name, $department)
 		{
 			try
@@ -78,7 +100,13 @@
 			}
 		}
 
-		//semester ändern
+		/**
+		 * Speichert Änderungen eines Semesters
+		 *
+		 * @param int $id ID des zu ändernen Semesters
+		 * @param int $language neue ID der Sprache
+		 * @param string $name neuer Name des Semesters
+		**/
 		public function updateSemester($id, $language, $name)
 		{
 			try
@@ -93,7 +121,11 @@
 			}
 		}
 
-		//semester entfernen
+		/**
+		 * Löscht ein Semester und alle zugehörigen Termine (zu Ändern in der Datenbank))
+		 *
+		 * @param int $id ID des zu löschenden Semesters
+		**/
 		public function removeSemester($id)
 		{
 			try
@@ -110,13 +142,18 @@
 		
 		//termine
 
-		//alle termine eines semesters ausgeben
+		/**
+		 * @param int $semester_id ID des Semesters
+		 *
+		 * @return array Gibt ein Array mit allen Terminen eines bestimmten Semesters zurück.
+		**/
 		public function getAppointments($semester_id)
 		{
 			try
 			{
 				$result = $this->connection->query("SELECT * FROM appointments
-														WHERE semester_id = $semester_id");
+														WHERE semester_id = $semester_id
+														ORDER BY date_from");
 				$resultSet = array();
 				if($result != null)
 					while($row = $result->fetch_assoc())
@@ -131,7 +168,12 @@
 			}
 		}
 
-		//alle termine eines semesters und einer bestimmten Nutzergruppe ausgeben
+		/**
+		 * @param int $semester_id ID des Semesters
+		 * @param char $eis Buchstabe der Zielgruppe (i = Interessent, e = Erstsemester, s = Student)
+		 *
+		 * @return array Gibt ein Array mit allen Terminen eines bestimmten Semesters für eine bestimmte Zielgruppe zurück.
+		**/
 		public function getAppointmentsForUsertype($semester_id, $eis)
 		{
 			try
@@ -145,7 +187,8 @@
 				}
 
 				$result = $this->connection->query("SELECT * FROM appointments
-														WHERE semester_id = $semester_id && $eis");
+														WHERE semester_id = $semester_id && $eis
+														ORDER BY date_from");
 				$resultSet = array();
 				if($result != null)
 					while($row = $result->fetch_assoc())
@@ -160,7 +203,18 @@
 			}
 		}
 
-		//termin zu einem semester hinzufügen
+		/**
+		 * Fügt einen neuen Termin ein
+		 *
+		 * @param int $language ID der Sprache
+		 * @param int $semester ID des Semesters
+		 * @param string $name Name des Termins
+		 * @param date $date_from Startdatum (YYYY-MM-TT)
+		 * @param date $date_to Enddatum (YYYY-MM-TT)
+		 * @param bool $interested für Interessenten?
+		 * @param bool $freshman für Erstsemester?
+		 * @param bool $student für Stundenten?
+		**/
 		public function insertAppointment($language, $semester, $name, $date_from, $date_to, $interested, $freshman, $student)
 		{
 			try
@@ -174,7 +228,18 @@
 			}
 		}
 
-		//termin ändern
+		/**
+		 * Speichert Änderungen eines Termins
+		 *
+		 * @param int $id ID des zu ändernen Termins
+		 * @param int $language neue ID der Sprache
+		 * @param string $name neuer Name des Termins
+		 * @param date $date_from neues Startdatum (YYYY-MM-TT)
+		 * @param date $date_to neues Enddatum (YYYY-MM-TT)
+		 * @param bool $interested für Interessenten?
+		 * @param bool $freshman für Erstsemester?
+		 * @param bool $student für Stundenten?
+		**/
 		public function updateAppointment($id, $language, $name, $date_from, $date_to, $interested, $freshman, $student)
 		{
 			try
@@ -189,7 +254,11 @@
 			}
 		}
 
-		//termin entfernen
+		/**
+		 * Löscht einen Termin
+		 *
+		 * @param int $id ID des zu löschenden Termins
+		**/
 		public function removeAppointment($id)
 		{
 			try
@@ -207,7 +276,9 @@
 
 		//zusatz
 
-		//alle fachbereiche auslesen
+		/**
+		 * @return array Gibt ein Array mit allen Fachbereichen (ID, Name) zurück
+		**/
 		public function getDepartments()
 		{
 			try
@@ -228,7 +299,11 @@
 			}
 		}
 
-		//fachbereich eines studienganges herausfinden
+		/**
+		 * @param string $name Name des Studienganges
+		 *
+		 * @return array Gibt ein Array mit dem Fachbereich des bestimmten Studienganges zurück
+		**/
 		public function getDepartmentFromStudycourse($name)
 		{
 			try
