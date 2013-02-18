1961cd24727d3a5abd455e3e6d2e679a7901af03
mrgrimmig <dkirchhof@gmail.com>
2013-02-18 11:28:03 +0100

    kontakte backend fabian + studiengänge backend okan

--- controllers/kontakteController.php
+++ controllers/kontakteController.php
@@ -4,7 +4,7 @@ class kontakteController {
 
 	public function __construct(){
 		//create new contact Model
-		require_once __DIR__ . '../../models/kontakteModel.php';
+		require_once '../../models/kontakteModel.php';
 		$this->contactsModel = new kontakteModel();
 	}
 
@@ -40,18 +40,19 @@ class kontakteController {
 	public function c_getContacts(){
 		return $this->contactsModel->m_getContacts();
 	}
-	
+
 	/**
 	*	Calls the model and returns all the departmentID of a specific course name
 	*	@return Array of Strings 
 	*/
 	public function c_getDeptByCourse($courseName){
-
-		$dept = $this->contactsModel->m_getDeptByCourse($courseName);
-		return $dept[0]['department_id'];
+		return $this->contactsModel->m_getContacts($courseName);
 	}
 
-
+	/**
+	*	Deletes a contact with specific id
+	*	@param $id ID of contact to delete
+	*/	
 	public function c_deleteContact($id){
 
 		$contactID = $id['contactID'];
@@ -60,6 +61,11 @@ class kontakteController {
 		$this->contactsModel->m_deleteContact($contactID, $deptID);
 	}
 
+	/**
+	*	Gets contact with specific id and all its attributes
+	*	@param $id ID of contact to get
+	*	@return Array of String
+	*/
 	public function c_getContact($id){
 
 		$contactID = $id['contactID']; 
@@ -68,12 +74,22 @@ class kontakteController {
 		return $this->contactsModel->m_getContact($contactID, $deptID);
 	}
 
+	/**
+	*	Gets the department(s) a specific contact is allocated to
+	*
+	*	@param $id $_POST object
+	*	@return Array of Strings 
+	*/
 	public function c_getContactDepts($id){
-		$contactID = $id['contactID'];
 
+		$contactID = $id['contactID'];
 		return $this->contactsModel->m_getContactDepts($contactID);
 	}
 
+	/**
+	*	Changes departmentID of a contact with specific id
+	*	@param $id ID of contact to change
+	*/
 	public function c_alterContact($id){
 		$contactID = $id['contactID'];
 		$deptID = $id['alterContactDepartment'];
@@ -81,6 +97,11 @@ class kontakteController {
 		$this->contactsModel->m_alterContact($contactID, $deptID);
 	}
 
+	/**
+	*	Alters a contact associated to multiple departments. alters the contact for all departments.
+	*
+	*	@param $id $_POST object
+	*/
 	public function c_alterAllContacts($id){
 
 		$contactID = $id['contactID'];
@@ -88,11 +109,13 @@ class kontakteController {
 		$this->contactsModel->m_alterAllContacts($contactID);
 	}
 
-	public function c_alterOneContact($id){
-		
-		$contactID = $id['contactID'];
-		$deptID = $id['deptID'];
+	/**
+	* Alters a contact associated to multiple departments. Alters the contact for the selected department only.
+	*
+	* @param $post $_POST object
+	*/
+	public function c_alterOneContact($post){
 
-		$this->contactsModel->m_alterOneContact($contactID, $deptID);
+		$this->contactsModel->m_alterOneContact($post);
 	}
 }
