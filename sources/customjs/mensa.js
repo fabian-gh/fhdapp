cd5bc28e8fa6f31f3da6c4cfced18c780de4be4c
Sascha M <...>
2013-02-19 12:48:39 +0100

    Zusammengefasst mit Daniel

--- sources/customjs/mensa.js
+++ sources/customjs/mensa.js
@@ -1,6 +1,68 @@
 $(document).ready(function(){
+	// Feiertags-Handling
+	$(".hol_text").hide();
 
-	//Datepicker formating
+	// wenn die feiertagsbezeichnung nicht leer
+	if($("#mon_hol_name").val() != ""){
+		// dann zeige das Textfeld
+		$(".mon_hol_text").show();
+		// und verstecke die restlichen Eingabefelder des Tages
+		$(".mon_col").hide();
+	}
+
+	if($("#tue_hol_name").val() != ""){
+		$(".tue_hol_text").show();
+		$(".tue_col").hide();
+	}
+
+	if($("#wed_hol_name").val() != ""){
+		$(".wed_hol_text").show();
+		$(".wed_col").hide();
+	}
+
+	if($("#thu_hol_name").val() != ""){
+		$(".thu_hol_text").show();
+		$(".thu_col").hide();
+	}
+
+	if($("#fri_hol_name").val() != ""){
+		$(".fri_hol_text").show();
+		$(".fri_col").hide();
+	}
+
+	// wenn Feiertags-Checkbox angeklickt wird
+	if($("#mon_hol").change(function(){
+		// setze die Bezeichnung bei jedem verstecken auf leer
+		$(".mon_hol_text").toggle().val("");
+		// und zeige bzw. verstecke die Eingabefelder des Tages
+		$(".mon_col").toggle();
+	}))
+
+	if($("#tue_hol").change(function(){
+		$(".tue_hol_text").toggle().val("");
+		$(".tue_col").toggle();
+	}))
+
+	if($("#wed_hol").change(function(){
+		$(".wed_hol_text").toggle().val("");
+		$(".wed_col").toggle();
+	}))
+
+	if($("#thu_hol").change(function(){
+		$(".thu_hol_text").toggle().val("");
+		$(".thu_col").toggle();
+	}))
+
+	if($("#fri_hol").change(function(){
+		$(".fri_hol_text").toggle().val("");
+		$(".fri_col").toggle();
+	}))
+
+
+	// Essen der Mensa Süd zu Beginn verstecken, da CAmpus Nord Standardwert
+	$(".south").hide();
+
+	// Datepicker-Formatierung
 	$("#start_date").click(function() {
 		$("#start_date").datepicker({ dateFormat: "dd.mm.yy", firstDay: 1, 
 									  dayNames: ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"],
@@ -9,12 +71,35 @@ $(document).ready(function(){
 	});
 
 
-	//Flip-Switch Handling
-	$('.south').hide();
-	$("#flip-2").on('slidestop', function(){
-		if($('.south').is(':hidden')){
-			$('.south').toggle();
+	// Radio Button control
+	// wenn Campus Süd angeklickt wird
+	$("#radio-south").click(function() {
+		// und die Mahlzeiten des Campus´ Süd versteckt sind
+		if($(".south").is(":hidden")){
+			// dann zeige diese
+			$(".south").show();
 		}
+	});
+
+	// wenn Campus Nord angeklickt wird
+	$("#radio-north").click(function() {
+		// verstecke die Süd-Mahlzeiten wieder
+		$(".south").hide();
+	});
+
+
+	// Submit-Button Handling
+	$("form").submit(function() {
+		// wenn das Startdatum leer
+	  	if ($("#start_date").val() == "") {
+	  		// zeige im span-Tag hinter dem Startdatum die Fehlermeldung
+		    $("#date_error").text("Bitte Startdatum eintragen").show();
+		    // und setze den Fokus auf das Startdatumsfeld
+		    $("#start_date").focus();
+		    // brich das Senden ab und verbleibe auf der Seite
+		    // mit true würde der Submit dennoch fortgesetzt werden
+		    return false;
+	  	}
 	});
 
 });
