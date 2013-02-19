7d7db0d1ad488440192780d3a6594d51e3106876
minh-gh <anh_minh@web.de>
2013-02-19 15:06:47 +0100

    Überschrift Frontend nach oben
    Leerzeile über der Überschrift entfernt: <h1> über den PHP-tag gepackt

--- views/faq/faq.php
+++ views/faq/faq.php
@@ -1,22 +1,18 @@
+<h1>FAQ</h1>
 <?php
-	//Controller einbinden
+	//Controller einbingen
 	require_once 'controllers/faqController.php';
-	//Controller-Objekt erstellen
 	$controller = new FaqController();
-	//Überschrift
-	echo "<h1>FAQ</h1>";
-	
-	//Auflistung der FAQs mit einem Collasible Set
-	echo "<div data-role='collapsible-set'>";								
 	//Bestimmung des Fachbereichs aus dem gewählten Studiengang
 	$dept = $controller->getDepartmentFromCourse($_GET['course']) ;
 	//Entsprechende FAQs auslesen
-	$resultSet = $controller->getFAQsFrontend($dept, $_GET['eis']);				
+	$resultSet = $controller->getFAQsFrontend($dept, $_GET['eis']);
+	//Collapsible-Set erstellen
+	echo "<div data-role='collapsible-set'>";								
 	//FAQs auflisten					
 	for($i=0; $i<count($resultSet); $i++) {
 		$frage = $resultSet[$i]['question'];
 		$antwort = $resultSet[$i]['answer'];
-		
 		if($i == 0)
 			echo "<div data-role='collapsible' data-iconpos='right' data-theme='a' data-collapsed-icon='arrow-r' data-expanded-icon='arrow-d' data-collapsed='false'><h1>$frage</h1><tr><td>$antwort</td></tr></div>"; //Erste FAQ aufklappen
 		else
