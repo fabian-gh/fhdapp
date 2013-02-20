155503edfd9ea595b20dff05ca8c045aef2f5263
minh-gh <anh_minh@web.de>
2013-02-20 13:15:02 +0100

    Frontend Fragen zur Bewerbung
    Erste 12 Fragen als Bewerberfragen. Sortierung muss daher auch ab 12
    beginnen.

--- views/faq/faq.php
+++ views/faq/faq.php
@@ -9,8 +9,18 @@
 	$resultSet = $controller->getFAQsFrontend($dept, $_GET['eis']);//Entsprechende FAQs auslesen
 	
 	//Collapsible-Set erstellen und FAQs auflisten
-	echo "<div data-role='collapsible-set' data-iconpos='right' data-collapsed-icon='arrow-r' data-expanded-icon='arrow-d' data-theme='a'>";												
-		for($i=0; $i<count($resultSet); $i++) {
+	echo "<div data-role='collapsible-set' data-iconpos='right' data-collapsed-icon='arrow-r' data-expanded-icon='arrow-d' data-theme='a'>";
+	if ($_GET['eis'] == 'i'){
+		echo "<h2>Fragen zur Bewerbung</h2>";
+		for($i=0; $i<11; $i++) {
+			$frage = $resultSet[$i]['question'];
+			$antwort = $resultSet[$i]['answer'];
+			echo "<div data-role='collapsible'><h1>$frage</h1><tr><td>$antwort</td></tr></div>";; //Rest zugeklappt lassen
+		}
+		echo "</br><h2>Fragen zum Studiengang</h2>";
+	}
+
+		for($i=11; $i<count($resultSet); $i++) {
 			$frage = $resultSet[$i]['question'];
 			$antwort = $resultSet[$i]['answer'];
 			if($i == 0)
