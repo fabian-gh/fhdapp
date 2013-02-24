4d6d5efc5b49095f72efb902dc62442f1ab19750
Fabian Martinovic <jimmy_crowbar@gmx.de>
2013-02-24 15:06:08 +0100

    utf8_decode in info.php

--- views/studiengaenge/info.php
+++ views/studiengaenge/info.php
@@ -6,10 +6,10 @@
 	$row = $controller->load_view();  
 
 	echo "<ul data-role='listview' data-dividertheme='a' data-inset='true'>
-	        <li data-role='list-divider'><h3>".$row['name']."</h3><p>".$row['graduate']."</p></li>
-	        <li><div style='font-size: 12pt'>Fachbereich: ".$row['department']."</div></li>                    
+	        <li data-role='list-divider'><h3>".utf8_decode($row['name'])."</h3><p>".utf8_decode($row['graduate'])."</p></li>
+	        <li><div style='font-size: 12pt'>Fachbereich: ".utf8_decode($row['department'])."</div></li>                    
 	        <li><div style='font-size: 12pt'>".$row['semestercount']." Semester</div></li> 
-	        <li data-theme='c'><div><a style='color:black; font-size: 10pt'>".$row['description']."</a></div></li>
+	        <li data-theme='c'><div><a style='color:black; font-size: 10pt'>".utf8_decode($row['description'])."</a></div></li>
 	        <li><a style='font-size: 12pt' href='".$row['link']."'>weitere Informationen</a></li>
 	    </ul>";
 
